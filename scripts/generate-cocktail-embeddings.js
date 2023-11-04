1e17d06898d1cfe731a74ee13adc546eec34dc4d
Jacob Nisnevich <jacob.nisnevich@gmail.com>
2023-11-04 13:53:01 -0700

    feat(): cocktail embeddings v1

--- scripts/generate-cocktail-embeddings.js
+++ scripts/generate-cocktail-embeddings.js
@@ -6,6 +6,57 @@ const ingredientEmbeddings = JSON.parse(
   fs.readFileSync(path.resolve(__dirname, '../data/ingredient-embeddings.json'), 'utf8')
 );
 
+const ozConversionRatios = {
+  '14.5-oz': 14.5,
+  barspoon: 0.1,
+  barspoons: 0.1,
+  bottle: 25.3605,
+  'bottle (750 mL)': 25.3605,
+  bunch: 0.1,
+  cluster: 0.1,
+  cup: 8,
+  'cup (scant)': 8,
+  cups: 8,
+  dahes: 0.1,
+  dash: 0.1,
+  dashes: 0.1,
+  drop: 0.1,
+  drops: 0.1,
+  'large scoop': 0.1,
+  lb: 0.1,
+  liter: 33.8141,
+  'medium pieces': 0.1,
+  mg: 0.1,
+  ml: 0.033814,
+  oz: 1,
+  'oz (scant)': 1,
+  piece: 0.1,
+  pinch: 0.1,
+  pinches: 0.1,
+  quart: 32,
+  quarter: 0.1,
+  scoop: 0.1,
+  slice: 0.1,
+  slices: 0.1,
+  'small piece': 0.1,
+  splash: 0.1,
+  splashes: 0.1,
+  spray: 0.1,
+  sprig: 0.1,
+  sprigs: 0.1,
+  sprinkle: 0.1,
+  sprinkles: 0.1,
+  stalks: 0.1,
+  stick: 0.1,
+  sticks: 0.1,
+  strip: 0.1,
+  swipes: 0.1,
+  tbsp: 0.5,
+  tsp: 0.166667,
+  'tsp (scant)': 0.166667,
+  '': 0.1
+};
+
 const multiplyVectorByScalar = (vector, scalar) => {
   return vector.map(component => component * scalar);
 };
@@ -15,9 +66,24 @@ const addVectors = (vectorA, vectorB) => {
 };
 
 const computeEmbedding = ingredientData => {
+  const maximumScalar = ingredientData.reduce((maximumSoFar, ingredient) => {
+    const { quantityAmount, quantityUnit } = ingredient;
+
+    const conversionMultiplier = ozConversionRatios[quantityAmount];
+    const scalar = parseFloat(quantityUnit) * conversionMultiplier;
+
+    return Math.max(maximumSoFar, scalar);
+  }, 0);
+
   const scaledVectors = ingredientData.map(ingredient => {
-    // For now 1:1 scale
-    return ingredientEmbeddings[ingredient.ingredientId].embedding;
+    const { ingredientId, quantityAmount, quantityUnit } = ingredient;
+    const { embedding } = ingredientEmbeddings[ingredientId];
+
+    const conversionMultiplier = ozConversionRatios[quantityAmount];
+    const scalar = (parseFloat(quantityUnit) * conversionMultiplier) / maximumScalar;
+
+    console.log(ingredientId, quantityAmount, quantityUnit, scalar, maximumScalar);
+    return multiplyVectorByScalar(embedding, scalar);
   });
 
   return scaledVectors.reduce(addVectors, new Array(scaledVectors[0].length).fill(0));
